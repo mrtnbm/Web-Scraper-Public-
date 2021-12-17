669c2394547d8be1461e8806bbd9e43de4f54305
mrtnbm <behmmartin14@googlemail.com>
2021-12-17 21:13:12 +0100

    added functionality for scraping one specific language

--- web-scraper-all.py
+++ web-scraper-all.py
@@ -11,22 +11,33 @@ MAX_RETRIES_REQ = 10
 print = sg.Print
 
 
-def parse_save_website(website_page):
+def parse_save_website(website_page, sel_lang=""):
     """
     Parses HTML with lxml (faster than html.parser,
     see https://www.crummy.com/software/BeautifulSoup/bs4/doc/#improving-performance) and only saves a tags into a
     list
+    :param sel_lang: selected language if not all get scraped
     :param website_page: Response of server request :returns: None
     """
-    for link_el in BeautifulSoup(website_page.content, features="lxml", parse_only=SoupStrainer('a')):
-        if hasattr(link_el, "href") and 'how-to-count-in' in link_el['href']:
-            # "aczu savnecze" is the first artificial language which we do not want to scrape
-            if 'aczu-savnecze' in link_el['href']:
-                break
-            lst_of_links.append(link_el['href'])
+    if sel_lang == "":
+        for link_el in BeautifulSoup(website_page.content, features="lxml", parse_only=SoupStrainer('a')):
+            if hasattr(link_el, "href") and 'how-to-count-in' in link_el['href']:
+                # "aczu savnecze" is the first artificial language which we do not want to scrape
+                if 'aczu-savnecze' in link_el['href']:
+                    break
+                lst_of_links.append(link_el['href'])
 
-    # the first link_el is for a random entry
-    lst_of_links.pop(0)
+        # the first link_el is for a random entry
+        lst_of_links.pop(0)
+    else:
+        for link_el in BeautifulSoup(website_page.content, features="lxml", parse_only=SoupStrainer('a')):
+            if hasattr(link_el, "href") and 'how-to-count-in' in link_el['href']:
+                if sel_lang.lower() in link_el['href']:
+                    lst_of_links.append(link_el['href'])
+                    break
+        if len(lst_of_links) == 0:
+            sg.SystemTray.notify("Language not listed on website", "Closing program...", display_duration_in_ms=1000,
+                                 fade_in_duration=200)
 
 
 def write_csv(string, path):
@@ -72,6 +83,18 @@ def progress_bar(counter):
             counter / (len(lst_of_links) - 1) * 100))
 
 
+def collapse(layout, key, visible):
+    """
+    Helper function that creates a Column that can be later made hidden, thus appearing "collapsed"
+    :param layout: The layout for the section
+    :param key: Key used to make this section visible / invisible
+    :param visible: visible determines if section is rendered visible or invisible on initialization
+    :return: A pinned column that can be placed directly into your layout
+    :rtype: sg.pin
+    """
+    return sg.pin(sg.Column(layout, key=key, visible=visible, pad=(0, 0)))
+
+
 def create_main_window():
     """
     Creates a GUI window in which the user can define the csv path and the numbers which should be extracted. The
@@ -80,6 +103,10 @@ def create_main_window():
     """
 
     sg.theme('Dark Gray 13')
+
+    hidden_sec = [[sg.Text("Specific language that should be scraped:", border_width=0)],
+                  [sg.InputText(default_text="German", text_color="lightgray",
+                                tooltip="Enter the language which should be scraped.", key="inputtxt", border_width=0)]]
 
     layout = [[sg.Text("Start number for each language:", border_width=0)],
               [sg.InputText(default_text=1, text_color="lightgray",
@@ -90,21 +117,27 @@ def create_main_window():
               [sg.Text("Steps between numbers", border_width=0)],
               [sg.InputText(default_text=1, text_color="lightgray",
                             tooltip="Enter in what steps numbers should be scraped between Start-End", border_width=0)],
-              [sg.Text("Path to CSV File", border_width=0)],
+              [sg.Text("Path to csv file:", border_width=0)],
               [sg.InputText(default_text=os.path.join('C', 'users', os.getlogin(), 'Documents'), text_color="lightgray",
                             tooltip="Enter desired path for the csv-file!", border_width=0), sg.FolderBrowse()],
+              [collapse(hidden_sec, 'hidden_sec', True)],
+              [sg.Checkbox(text="Scrape all languages", default=False, key='-CB-', enable_events=True)],
               [sg.Button("OK", font=("Helvetica", "10", "bold"), border_width=0), sg.Button("Exit", border_width=0)]]
 
     window = sg.Window('Web-Scraper', layout, finalize=True, resizable=True, auto_size_text=True,
                        auto_size_buttons=True, no_titlebar=True, grab_anywhere=True, keep_on_top=True)
 
-    alt_value = ""
+    toggle_sec1 = True
+
     while True:
         event, values = window.read()
         if event in (sg.WIN_CLOSED, 'Exit'):
             window.close()
             sys.exit(1)
-        elif event == "OK":
+        if event == "-CB-":
+            toggle_sec1 = not toggle_sec1
+            window['hidden_sec'].update(visible=toggle_sec1)
+        if event == "OK":
             window.close()
             while True:
                 try:
@@ -117,7 +150,8 @@ def create_main_window():
                     alt_values = sg.popup_get_text(
                         "Error: Values contains non integer values. Please enter integer values:", default_text="1,2,1",
                         no_titlebar=True, keep_on_top=True, grab_anywhere=True)
-                    if (values[0] or values[1] or values[2]) is None or "" or alt_values is None:
+                    if (values[0] or values[1] or values[2] or (
+                            not is_alllang and values["inputtxt"])) is None or "" or alt_values is None:
                         sg.SystemTray.notify("Cancelled", "Closing program...", display_duration_in_ms=1000,
                                              fade_in_duration=200)
                         sys.exit(1)
@@ -127,6 +161,8 @@ def create_main_window():
                         values[0] = alt_values_split[0]
                         values[1] = alt_values_split[1]
                         values[2] = alt_values_split[2]
+                        if not is_alllang:
+                            values[4] = alt_values_split[4]
                     except IndexError as erri:
                         sg.SystemTray.notify("Too many retries", "Closing program...", display_duration_in_ms=1000,
                                              fade_in_duration=200)
@@ -138,11 +174,17 @@ def create_main_window():
             return event, values
 
 
-def progress_bar_meter(count):
+def progress_bar_meter(counter):
     sg.theme('Dark Gray 13')
-    return sg.one_line_progress_meter('Scraping numerals...', count + 1, len(lst_of_links), 'Scraping numerals...',
+    if selected_lang == "":
+        return sg.one_line_progress_meter('Scraping numerals...', counter + 1, len(lst_of_links), 'Scraping numerals...',
                                       key='key', no_button=True, orientation='h', no_titlebar=True, grab_anywhere=True,
                                       keep_on_top=True)
+    else:
+        return sg.one_line_progress_meter('Scraping numerals...', counter + 1, len(range(start, end, step)), 'Scraping numerals...',
+                                          key='key', no_button=True, orientation='h', no_titlebar=True,
+                                          grab_anywhere=True,
+                                          keep_on_top=True)
 
 
 if __name__ == '__main__':
@@ -151,6 +193,8 @@ if __name__ == '__main__':
     end = int(values[1])
     step = int(values[2])
     file_path = values[3]
+    selected_lang = str(values["inputtxt"])
+    is_alllang = values["-CB-"]
     retries = 0
 
     start_time = time.time()  # log execution time of script
@@ -191,15 +235,19 @@ if __name__ == '__main__':
     lst_of_links = []
     lst_of_words = ""
 
-    parse_save_website(page)
+    parse_save_website(page, sel_lang=selected_lang)
 
     count = 0
+    count_inner = 0
     retries = 0
     for link in lst_of_links:
-        progress_bar_meter(count)
+        if selected_lang == "":
+            progress_bar_meter(count)
         lang = find_lang_short(link)
 
         for i in range(start, end, step):
+            if not selected_lang == "":
+                progress_bar_meter(count_inner)
             # requests.Session uses single TCP-connection for sending/receiving HTTP multi reqs/resps
             # saves time over opening a new connection for every single req/resp pair, see
             # https://en.wikipedia.org/wiki/HTTP_persistent_connection
@@ -246,6 +294,7 @@ if __name__ == '__main__':
                     else:
                         lst_of_words = lst_of_words + find_lang_long(link) + ";" + str(i) + ";" + \
                                        soup.get_text(separator=" ", strip=True).split(':')[-1] + "\n"
+            count_inner += 1
         count += 1
     alt_path = ""
     retries = 0
