67ed651a280443c12b78805b71728df59a9033bb
mrtnbm <behmmartin14@googlemail.com>
2023-03-28 01:55:17 +0200

    create virtual display in order to test system tray notifications

--- test_web_scraper_all.py
+++ test_web_scraper_all.py
@@ -1,5 +1,7 @@
 import unittest
 import os
+os.system('Xvfb :1 -screen 0 1600x1200x16  &')    # create virtual display with size 1600x1200 and 16 bit color. Color can be changed to 24 or 8
+os.environ['DISPLAY']=':1.0'    # tell X clients to use our virtual DISPLAY :1.0.
 
 from unittest.mock import Mock
 
